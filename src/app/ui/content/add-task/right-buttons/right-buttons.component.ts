044eeef97e8d507094998f5124ef69314e10d832
Ravindra Devrani <ravindra.devrani003@gmail.com>
2023-04-17 23:25:12 +0530

    right side button dropdown toggle added

--- src/app/ui/content/add-task/right-buttons/right-buttons.component.ts
+++ src/app/ui/content/add-task/right-buttons/right-buttons.component.ts
@@ -1,22 +1,28 @@
 import { Component } from '@angular/core';
 
+export enum RightSideButtons{
+  'DUE_DATE'=1,
+  'REMIND_ME',
+  'REPEAT'
+}
+
 @Component({
   selector: 'app-right-buttons',
   template: `
     <!--right side button container -->
     <div class="absolute top-0 right-0 h-full flex items-center">
               
-      <!-- add new date button -->
+      <!-- add due date button -->
       <div class="relative">
-        <button class="h-full w-10 flex items-center justify-center rounded-r-lg hover:bg-gray-200 transition duration-300 tooltip" data-tip="Add new date" style="margin-left: 8px;" (click)="toggleDropdown()">
+        <button class="h-full w-10 flex items-center justify-center rounded-r-lg hover:bg-gray-200 transition duration-300 tooltip" data-tip="Add due date" style="margin-left: 8px;" (click)="toggleDropdown(rightSideButtons.DUE_DATE)">
           <i class="far fa-calendar-alt"></i>
         </button>
 
-        <div *ngIf="showDropdown" class="absolute bottom-8 right-0 mb-0.5 mr-4 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100">
+        <div *ngIf="activeButton===rightSideButtons.DUE_DATE.toString()" class="absolute bottom-8 right-0 mb-0.5 mr-4 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100">
           <div class="py-1" >
             <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Today</a>
             <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Tomorrow</a>
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Next week</a>
+            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Pick a date</a>
           </div>
         </div>
       </div>
@@ -24,12 +30,12 @@ import { Component } from '@angular/core';
       <!-- remind me button -->
 
       <div class="relative">
-        <button (click)="toggleDropdown()" class="h-full w-10 flex items-center justify-center rounded-r-lg hover:bg-gray-200 transition duration-300 tooltip" data-tip="Remind me">
+        <button (click)="toggleDropdown(rightSideButtons.REMIND_ME)" class="h-full w-10 flex items-center justify-center rounded-r-lg hover:bg-gray-200 transition duration-300 tooltip" data-tip="Remind me">
            <i class="far fa-clock"></i>
         </button>
 
         <!-- dropdown menu -->
-        <div *ngIf="showDropdown" class="absolute bottom-8 right-0 mb-0.5 mr-4 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100">
+        <div *ngIf="activeButton===rightSideButtons.REMIND_ME.toString()" class="absolute bottom-8 right-0 mb-0.5 mr-4 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100">
           <div class="py-1" >
             <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Today</a>
             <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Tomorrow</a>
@@ -40,15 +46,15 @@ import { Component } from '@angular/core';
 
 
 
-              <!-- repeat button -->
+      <!-- repeat button -->
              
       <div class="relative">
-        <button (click)="toggleDropdown()" class="h-full w-10 flex items-center justify-center rounded-r-lg hover:bg-gray-200 transition duration-300 tooltip" data-tip="repeat">
+        <button (click)="toggleDropdown(rightSideButtons.REPEAT)" class="h-full w-10 flex items-center justify-center rounded-r-lg hover:bg-gray-200 transition duration-300 tooltip" data-tip="repeat">
            <i class="fas fa-repeat"></i>
         </button>
 
         <!-- dropdown menu -->
-        <div *ngIf="showDropdown" class="absolute bottom-8 right-0 mb-0.5 mr-4 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100">
+        <div *ngIf="activeButton===rightSideButtons.REPEAT.toString()" class="absolute bottom-8 right-0 mb-0.5 mr-4 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100">
           <div class="py-1" >
             <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Today</a>
             <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Tomorrow</a>
@@ -63,9 +69,13 @@ import { Component } from '@angular/core';
   ]
 })
 export class RightButtonsComponent {
-  showDropdown = false;
-
-  toggleDropdown() {
-    this.showDropdown = !this.showDropdown;
+  activeButton = "";
+  rightSideButtons = RightSideButtons;
+ 
+  toggleDropdown(button: RightSideButtons) {
+    if (this.activeButton === button.toString())
+      this.activeButton = ""
+    else
+      this.activeButton=button.toString()
   }
 }
