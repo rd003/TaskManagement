8caf9db17f149c26bf14dab75338dcf46f5b52e5
Ravindra Devrani <ravindra.devrani003@gmail.com>
2023-04-18 23:02:02 +0530

    due date ui added

--- src/app/ui/content/add-task/right-buttons/right-buttons.component.ts
+++ src/app/ui/content/add-task/right-buttons/right-buttons.component.ts
@@ -6,26 +6,32 @@ export enum RightSideButtons{
   'REPEAT'
 }
 
+
+
 @Component({
   selector: 'app-right-buttons',
   template: `
     <!--right side button container -->
-    <div class="absolute top-0 right-0 h-full flex items-center">
+    <div class="absolute top-0 right-0 h-full flex space-x-3 mr-2 items-center">
               
       <!-- add due date button -->
       <div class="relative">
-        <button class="h-full w-10 flex items-center justify-center rounded-r-lg hover:bg-gray-200 transition duration-300 tooltip" data-tip="Add due date" style="margin-left: 8px;" (click)="toggleDropdown(rightSideButtons.DUE_DATE)">
-          <i class="far fa-calendar-alt"></i>
+        <button class="h-full space-x-1 flex items-center justify-center rounded-r-lg hover:bg-gray-200 transition duration-300 tooltip" data-tip="Add due date" (click)="toggleDropdown(rightSideButtons.DUE_DATE)">
+            <i class="far fa-calendar-alt"></i>
+            <span *ngIf="selectedDueDate.value">
+                 {{selectedDueDate.label}}
+           </span>
+         
         </button>
         
         <!-- add due date dropdown -->
         <div *ngIf="activeButton===rightSideButtons.DUE_DATE.toString()" class="absolute bottom-8 right-0 mb-0.5 mr-4 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100">
           <div class="py-1" >
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Today</a>
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Tomorrow</a>
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Next week</a>
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Pick a date</a>
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Remove due date</a>
+            <a *ngFor="let dropdown of dueDateDropdownValues" (click)="selectDueDate(dropdown)" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">
+               {{dropdown.label}}
+            </a>
+           
+            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" (click)="removeDueDate()">Remove due date</a>
           </div>
         </div>
       </div>
@@ -33,7 +39,7 @@ export enum RightSideButtons{
       <!-- remind me button -->
 
       <div class="relative">
-        <button (click)="toggleDropdown(rightSideButtons.REMIND_ME)" class="h-full w-10 flex items-center justify-center rounded-r-lg hover:bg-gray-200 transition duration-300 tooltip" data-tip="Remind me">
+        <button (click)="toggleDropdown(rightSideButtons.REMIND_ME)" class="h-full flex items-center justify-center rounded-r-lg hover:bg-gray-200 transition duration-300 tooltip" data-tip="Remind me">
            <i class="far fa-clock"></i>
         </button>
 
@@ -54,7 +60,7 @@ export enum RightSideButtons{
       <!-- repeat button -->
              
       <div class="relative">
-        <button (click)="toggleDropdown(rightSideButtons.REPEAT)" class="h-full w-10 flex items-center justify-center rounded-r-lg hover:bg-gray-200 transition duration-300 tooltip" data-tip="repeat">
+        <button (click)="toggleDropdown(rightSideButtons.REPEAT)" class="h-full  flex items-center justify-center rounded-r-lg hover:bg-gray-200 transition duration-300 tooltip" data-tip="repeat">
            <i class="fas fa-repeat"></i>
         </button>
 
@@ -79,6 +85,53 @@ export enum RightSideButtons{
 export class RightButtonsComponent {
   activeButton = "";
   rightSideButtons = RightSideButtons;
+  selectedDueDate = {label:"",value:""};
+  dueDateDropdownValues = [
+    {
+      label: 'Today', value: () => {
+        const today = new Date();
+        return today.toISOString();
+      }
+    },
+    {
+      label: 'Tommorow',
+      value: () => {
+        const date = new Date();
+        date.setDate(date.getDate() + 1);
+        return date.toISOString();;
+      }
+    },
+    {
+      label: 'Next Week',
+      value: () => {
+        const date = new Date();
+        date.setDate(date.getDate() + 7);
+        return date.toISOString();;
+      }
+    },
+    {
+      label: 'Pick a date',
+      value: () => {
+         return ""
+      }
+    },
+  ]
+
+  hideDropdown() {
+    this.activeButton = "";
+  }
+
+  selectDueDate(dueDate:{ label: string; value: () => string; }) {
+    this.selectedDueDate = {label:dueDate.label,value:dueDate.value()};
+    // hide the open drop down menu
+    this.hideDropdown();
+  }
+
+  removeDueDate() {
+    this.selectedDueDate = { label: "", value: "" };
+    this.hideDropdown();
+    
+  }
  
   toggleDropdown(button: RightSideButtons) {
     if (this.activeButton === button.toString())
