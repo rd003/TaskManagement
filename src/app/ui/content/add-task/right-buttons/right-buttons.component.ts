9477b07b0e8c25f6c2e52a3c48e7d05b88dab032
Ravindra Devrani <ravindra.devrani003@gmail.com>
2023-04-18 19:43:10 +0530

    added static options to task input dropdowns

--- src/app/ui/content/add-task/right-buttons/right-buttons.component.ts
+++ src/app/ui/content/add-task/right-buttons/right-buttons.component.ts
@@ -17,12 +17,15 @@ export enum RightSideButtons{
         <button class="h-full w-10 flex items-center justify-center rounded-r-lg hover:bg-gray-200 transition duration-300 tooltip" data-tip="Add due date" style="margin-left: 8px;" (click)="toggleDropdown(rightSideButtons.DUE_DATE)">
           <i class="far fa-calendar-alt"></i>
         </button>
-
+        
+        <!-- add due date dropdown -->
         <div *ngIf="activeButton===rightSideButtons.DUE_DATE.toString()" class="absolute bottom-8 right-0 mb-0.5 mr-4 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100">
           <div class="py-1" >
             <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Today</a>
             <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Tomorrow</a>
+            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Next week</a>
             <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Pick a date</a>
+            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Remove due date</a>
           </div>
         </div>
       </div>
@@ -34,12 +37,14 @@ export enum RightSideButtons{
            <i class="far fa-clock"></i>
         </button>
 
-        <!-- dropdown menu -->
+        <!--remind me dropdown menu -->
         <div *ngIf="activeButton===rightSideButtons.REMIND_ME.toString()" class="absolute bottom-8 right-0 mb-0.5 mr-4 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100">
           <div class="py-1" >
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Today</a>
+            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Later Today</a>
             <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Tomorrow</a>
             <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Next week</a>
+            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Pick a date</a>
+            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Remove reminder</a>
           </div>
         </div>
       </div>
@@ -53,12 +58,15 @@ export enum RightSideButtons{
            <i class="fas fa-repeat"></i>
         </button>
 
-        <!-- dropdown menu -->
+        <!-- repeat dropdown menu -->
         <div *ngIf="activeButton===rightSideButtons.REPEAT.toString()" class="absolute bottom-8 right-0 mb-0.5 mr-4 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100">
           <div class="py-1" >
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Today</a>
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Tomorrow</a>
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Next week</a>
+            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Daily</a>
+            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Week days</a>
+            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Weekly</a>
+            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Monthly</a>  
+            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Custom</a>  
+            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">Never repeat</a>   
           </div>
         </div>
       </div>
