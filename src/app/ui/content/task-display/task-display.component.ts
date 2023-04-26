ad837233763d679556fd5327c7ea8b6cfc717a93
Ravindra Devrani <ravindra.devrani003@gmail.com>
2023-04-26 17:14:31 +0530

    diplay category below task, ui added

--- src/app/ui/content/task-display/task-display.component.ts
+++ src/app/ui/content/task-display/task-display.component.ts
@@ -4,12 +4,17 @@ import { TaskModel } from 'src/app/models/task.model';
 @Component({
   selector: 'app-task-display',
   template: `
-          <div *ngFor="let task of tasks" class="py-3 px-4 my-1 bg-gray-200 rounded-lg flex">
+          <div *ngFor="let task of tasks" class="py-2 px-4 my-1 bg-gray-200 rounded-lg flex">
                <div class="radio-container mr-2">
                   <input type="radio" (click)="toggleTaskEvent.emit(task)" class="radio"/>
                </div>  
-               <div class="task-container" [ngClass]="{'line-through':task.completed}">
-                  {{task.title}} 
+               <div class="task-container flex flex-col" [ngClass]="{'line-through':task.completed}">
+                   <div>
+                     {{task.title}} 
+                   </div>
+                   <div *ngIf="displayCategory">
+                      category
+                   </div>
                </div>   
                <div class="star-container ml-auto">
                    <i class="fa-star cursor-pointer" [ngClass]="{'fa-solid':task.isImportant,'fa-regular':!task.isImportant}" (click)="toggleMarkImportant.emit(task)"></i>
@@ -24,5 +29,6 @@ export class TaskDisplayComponent {
    @Input() tasks!: ReadonlyArray<TaskModel>;
    @Output() toggleTaskEvent = new EventEmitter<TaskModel>();
    @Output() toggleMarkImportant = new EventEmitter<TaskModel>();
+   @Input() displayCategory = true;
 
 }
